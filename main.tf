29cd717005c161c5be3e7d5e0647b2d4494c28e7
Réda Maizate <reda.maizate@affluences.com>
2022-03-03 00:16:30 +0100

    Added a vm_image to test

--- main.tf
+++ main.tf
@@ -2,6 +2,11 @@ resource "google_notebooks_instance" "get_insta_data_tf" {
   name         = "get_insta_data_tf"
   location     = "us-west1-b"
   machine_type = "n1-standard-1"
+  vm_image {
+    project      = "deeplearning-platform-release"
+    image_family = "tf-latest-cpu"
+  }
+
 }
 
 resource "google_storage_bucket" "reda-bucket" {
