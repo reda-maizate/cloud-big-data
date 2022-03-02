bcdb6b807e02cec8bdb7e1fdad1b3b6f7fe97fb9
Réda Maizate <reda.maizate@affluences.com>
2022-03-03 00:11:03 +0100

    Changed name of google_notebooks_instance

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_notebooks_instance" "instance" {
+resource "google_notebooks_instance" "get_insta_data_tf" {
   name         = "get_insta_data_tf"
   location     = "us-west1-b"
   machine_type = "n1-standard-1"
