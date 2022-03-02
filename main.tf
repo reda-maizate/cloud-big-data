43ab13dc70116c4542e061cb429f0f813a023813
Réda Maizate <reda.maizate@affluences.com>
2022-03-03 00:04:21 +0100

    Changed the structure

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "google_notebooks_instance" "instance" {
+  name         = "get_insta_data_tf"
+  location     = "us-west1-b"
+  machine_type = "n1-standard-1"
+}
+
+resource "google_storage_bucket" "reda-bucket" {
+  name                        = "reda-bucket-tf"
+  location                    = "EU"
+  uniform_bucket_level_access = true
+}
