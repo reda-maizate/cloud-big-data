f75f6a487984c4fa1f33d3771a0549a1fbb7bf17
Réda Maizate <reda.maizate@affluences.com>
2022-03-04 00:30:15 +0100

    Removed var credentials

--- providers.tf
+++ providers.tf
@@ -1,5 +1,4 @@
 provider "google" {
-  credentials = file("/Users/redamaizate/Documents/4IABD/Cloud_Big_Data/cloud-big-data-335413-baee6b95bd04.json")
   project     = var.project_id
   region      = var.region
   zone        = var.zone
